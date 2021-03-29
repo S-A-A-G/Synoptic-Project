c7b5e79bd3ef891815962c3fbada0e1939736097
ProTheKiwiKingdom <54159049+ProTheKiwiKingdom@users.noreply.github.com>
2021-03-29 06:47:33 +0100

    Add In Colour Picker

---
+++ Assets/Scipts/UI/HSVPicker/ExampleScripts/ColorPickerTester.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HSVPicker.Examples
+{
+    public class ColorPickerTester : MonoBehaviour 
+    {
+
+
+
+        public SpriteRenderer sprite;
+      
+        public ColorPicker picker;
+
+        public Color Color = Color.white;
+        public bool SetColorOnStart = false;
+
+
+	    // Use this for initialization
+	    void Start () 
+        {
+            
+            picker.onValueChanged.AddListener(color =>
+            {
+                //render.material.color = color;
+                sprite.color = color;
+                Color = color;
+            });
+
+            //render.material.color = picker.CurrentColor;
+            sprite.color = picker.CurrentColor;
+            if (SetColorOnStart) 
+            {
+                picker.CurrentColor = Color;
+            }
+ 
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+	
+          
+        }
+    }
+}
